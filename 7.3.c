f6103741940eb81030f0ee0d718fee21d30f2189
Void_Empty <87643822+Voemp@users.noreply.github.com>
2022-11-15 22:37:05 +0800

    Add files via upload

---
+++ 7.3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int max(int, int); //函数声明
+int main() {
+    int a, b;
+    printf("Please enter two number:");
+    scanf("%d%d", &a, &b);
+    printf("max = %d", max(a, b));
+    return 0;
+}
+
+//函数定义
+int max(int a, int b) {
+    return a > b ? a : b;
+}
---
+++ 7.4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int LCM(int, int);
+
+int main() {
+    int a, b;
+    printf("Input a b:");
+    scanf("%d%d", &a, &b);
+    printf("LCM = %d", LCM(a, b));
+    return 0;
+}
+
+//LCM函数功能：用穷举法计算两个正整数的最小公倍数
+int LCM(int a, int b) {
+    if (a < b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+    for (int i = 2; i > 0; ++i) {
+        if (a * i % b == 0)
+            return a * i;
+    }
+}
