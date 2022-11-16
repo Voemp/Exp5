314e1a01ca9ef51f08afcceef145f8a2ab6f9cc6
Void_Empty <87643822+Voemp@users.noreply.github.com>
2022-11-16 11:47:14 +0800

    Add files via upload

---
+++ 7.13_PrimeNumber/Function.h
@@ -0,0 +1,9 @@
+// Created by VoidEmpty on 2022/11/15.
+#ifndef INC_7_13_PRIMENUMBER_FUNCTION_H
+#define INC_7_13_PRIMENUMBER_FUNCTION_H
+void Function1();
+void Function2();
+void Function3();
+void Function4();
+void Function5();
+#endif //INC_7_13_PRIMENUMBER_FUNCTION_H
